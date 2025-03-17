c9e9939f63a9ea3fdd7dc0b64adc07d4318e92ba
songrui2008 <35618285+songrui2008@users.noreply.github.com>
2025-03-17 21:32:09 +0800

    Create diy-part2.sh

---
+++ scripts/m2/diy-part2.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+#
+# Copyright (c) 2019-2020 P3TERX <https://p3terx.com>
+#
+# This is free software, licensed under the MIT License.
+# See /LICENSE for more information.
+#
+# https://github.com/P3TERX/Actions-OpenWrt
+# File name: diy-part2.sh
+# Description: OpenWrt DIY script part 2 (After Update feeds)
+
+# DDNS.to
+git_sparse_clone main https://github.com/linkease/nas-packages-luci luci/luci-app-ddnsto
+git_sparse_clone master https://github.com/linkease/nas-packages network/services/ddnsto
+
+# iStore
+git_sparse_clone main https://github.com/linkease/istore-ui app-store-ui
+git_sparse_clone main https://github.com/linkease/istore luci
+
+#优先安装 passwall 源
+./scripts/feeds install -a -f -p passwall_packages
+./scripts/feeds install -a -f -p passwall_luci
