a5862b08c4df6882076574b62e0e394d7c263c9a
songrui2008 <35618285+songrui2008@users.noreply.github.com>
2025-03-17 23:53:19 +0800

    Update diy-part2.sh

--- scripts/m2/diy-part2.sh
+++ scripts/m2/diy-part2.sh
@@ -14,8 +14,8 @@ git_sparse_clone main https://github.com/linkease/nas-packages-luci luci/luci-ap
 git_sparse_clone master https://github.com/linkease/nas-packages network/services/ddnsto
 
 # iStore
-git_sparse_clone main https://github.com/linkease/istore-ui app-store-ui
-git_sparse_clone main https://github.com/linkease/istore luci
+#git_sparse_clone main https://github.com/linkease/istore-ui app-store-ui
+#git_sparse_clone main https://github.com/linkease/istore luci
 
 #优先安装 passwall 源
 ./scripts/feeds install -a -f -p passwall_packages
