3a88ea3f79a3358b3d89743758fbf468baa6c3a0
songrui2008 <35618285+songrui2008@users.noreply.github.com>
2025-03-18 10:58:32 +0800

    Update diy-part3.sh

--- scripts/m2/diy-part3.sh
+++ scripts/m2/diy-part3.sh
@@ -14,7 +14,7 @@
 sed -i 's/192.168.1.1/192.168.100.1/g' package/base-files/files/bin/config_generate
 
 #修改版本信息
-sed -i "s/DISTRIB_DESCRIPTION='*.*'/DISTRIB_DESCRIPTION='SnowR'/g"  package/base-files/files/etc/openwrt_release
+sed -i "s/DISTRIB_DESCRIPTION='*'/DISTRIB_DESCRIPTION='SnowR'/g"  package/base-files/files/etc/openwrt_release
 
 #修改显示型号
 #sed -i "s/board_name = .*/board_name = 'CMIOT AX18',/g" feeds/luci/modules/luci-base/luasrc/sys.lua
