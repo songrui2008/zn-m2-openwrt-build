1361a546bba19e33fa706e1815076c37d9ed0831
songrui2008 <35618285+songrui2008@users.noreply.github.com>
2025-04-08 21:01:19 +0800

    Update diy-part2.sh

--- scripts/m2-istore/diy-part2.sh
+++ scripts/m2-istore/diy-part2.sh
@@ -6,9 +6,16 @@
 # See /LICENSE for more information.
 #
 # https://github.com/P3TERX/Actions-OpenWrt
-# File name: diy-part1.sh
-# Description: OpenWrt DIY script part 1 (Before Update feeds)
-#
+# File name: diy-part2.sh
+# Description: OpenWrt DIY script part 2 (After Update feeds)
 
-# Uncomment a feed source
-#echo 'src-git custom https://github.com/xiaorouji/openwrt-passwall.git;packages' >>feeds.conf.default
+
+
+# iStore
+#git_sparse_clone main https://github.com/linkease/istore-ui app-store-ui
+#git_sparse_clone main https://github.com/linkease/istore luci
+echo >> feeds.conf.default
+echo 'src-git istore https://github.com/linkease/istore;main' >> feeds.conf.default
+./scripts/feeds update istore
+./scripts/feeds install -d y -p istore luci-app-store
+
